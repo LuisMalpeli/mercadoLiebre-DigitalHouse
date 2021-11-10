da9b1e6ff841de8308fc5a21afae60381fe3534d
Luis <malpeliluis@gmail.com>
2021-11-10 20:11:32 -0300

    mercado liebre

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use(express.static('public'));
+
+
+app.listen(process.env.PORT || port, ()=>{
+    console.log('Servidor corriendo en el puerto : ',port);
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
